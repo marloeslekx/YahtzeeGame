b192dbafb2f6882c5196f6813e2aeb915dd01bc3
marloeslekx <marloeslekx@gmail.com>
2019-07-12 22:07:20 +0200

    Small change

--- Yahtzee/src/Yahtzee/Application.java
+++ Yahtzee/src/Yahtzee/Application.java
@@ -3,9 +3,9 @@ package Yahtzee;
 public class Application {
 
 	public static void main(String[] args) {
-	YahtzeeSpel speler1 = new YahtzeeSpel();
+	YahtzeeSpel spel = new YahtzeeSpel();
 	
-	speler1.spelen();
+	spel.spelen();
 	
 	}
 }
